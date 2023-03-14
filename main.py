6fa090f4c5305089abd230add9cef69f97cc557a
StreetDanceD313 <dudu020300@gmail.com>
2023-03-14 18:54:03 -0300

    primeira versão do código

---
+++ main.py
@@ -0,0 +1,9 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+print("Meu sistema lê dois valores e mostra a soma deles.")
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo  número:"))
+soma = num1 + num2
+print("A soma dos números é:" + str(soma))
+
